987fa70f5574a2e23b9f40ab4e7c70de4b9568f1
Ramsteal123 <evankeylon@gmail.com>
2026-04-16 16:58:40 +0000

    finished extra 3 javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // add javascript here
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
